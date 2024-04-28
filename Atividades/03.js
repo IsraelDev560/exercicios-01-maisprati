2d82ad6de95ac6f3c66b6abc2a3b009d26774142
IsraelDev560 <devisrael560@gmail.com>
2024-04-28 00:49:49 -0300

    Algoritmo: 'Ler quatros números com funções diferentes'

--- Atividades/03.js
+++ Atividades/03.js
@@ -7,15 +7,22 @@
 
 const prompt = require("prompt-sync")()
 
-let n1 = parseInt(prompt("Digite o Primeiro número "));
-let n2 = parseInt(prompt("Digite o Segundo número "));
-let n3 = parseInt(prompt("Digite o Terceiro número "));
-let n4 = parseInt(prompt("Digite o Quarto número "));
+let n1 = parseInt(prompt("Digite o Primeiro número: "));
+let n2 = parseInt(prompt("Digite o Segundo número: "));
+let n3 = parseInt(prompt("Digite o Terceiro número: "));
+let n4 = parseInt(prompt("Digite o Quarto número: "));
 
-n4 += n1+n2+n3;
-let soma = n1 + 25;
-let multiplica = n2 * 3;
-let porcentagem = 0.12 * n3;
+if (isNaN(n1 && n2 && n3 && n4)) {
+    console.log("Digite apenas números.");
+} else {
+    n4 += n1 + n2 + n3;
+    let soma = n1 + 25;
+    let multiplica = n2 * 3;
+    let porcentagem = 0.12 * n3;
 
+    console.log(soma);
+    console.log(multiplica);
+    console.log(porcentagem);
+    console.log(n4);
+}
 
-console.log(soma,multiplica,porcentagem,n4)
