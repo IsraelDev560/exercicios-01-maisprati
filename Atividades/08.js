af721978a54e630e6a1bec513f6c482de38ae946
IsraelDev560 <devisrael560@gmail.com>
2024-04-28 01:09:36 -0300

    Algoritmo: 'Imprimir Números em ordem Crescente'

--- Atividades/08.js
+++ Atividades/08.js
@@ -6,12 +6,12 @@ const prompt = require("prompt-sync")()
 let num1 = parseInt(prompt("Digite o Primeiro Número "));
 let num2 = parseInt(prompt("Digite o Segunda Número "));
 
-if(num1 == num2){
+if (num1 == num2) {
     console.log("os valores são iguais");
-}else if(num1 > num2){
-    console.log(num2,num1);
-}else if(isNaN(num1 && num2)){
-    console.log("Isso não é um número")
-}else {
-    console.log(num1,num2);
+} else if (num1 > num2) {
+    console.log(num2, num1);
+} else if (isNaN(num1 && num2)) {
+    console.log("Isso não é um número");
+} else {
+    console.log(num1, num2);
 }
