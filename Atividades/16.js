5b26c3a21d5cac17a37c13120f63490837559a3e
IsraelDev560 <devisrael560@gmail.com>
2024-04-28 01:47:01 -0300

    Algoritmo: 'Imprimir os 50 primeiros números primos acima de 100'

--- Atividades/16.js
+++ Atividades/16.js
@@ -1,7 +1,6 @@
 // 16. Escreva um algoritmo para imprimir os 50 primeiros número primos maior que 100.
 // Obs.: Número primo é aquele divisível somente por 1 e ele mesmo.
 
-
 let numero = 101;
 let contador = 0;
 let limite = 50;
