d3a3315be165ef81f6c63be5db735bac0dc607e2
IsraelDev560 <devisrael560@gmail.com>
2024-04-28 01:20:52 -0300

    Algortimo: 'Imprimir números de 1000 a 1999'

--- Atividades/12.js
+++ Atividades/12.js
@@ -3,9 +3,9 @@
 
 let numero = 1000;
 
-while(numero < 1999){
- 
-    if (numero % 11 == 5){
-        console.log(numero)
-    }   numero++;
+while (numero <= 1999) {
+
+    if (numero % 11 == 5) {
+        console.log(numero);
+    } numero++;
 }
